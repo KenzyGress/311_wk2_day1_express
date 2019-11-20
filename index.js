8ada955ae8a7cc5caa18e30d0915ace8158198e1
KenzyGress <mleegress@gmail.com>
2019-11-19 22:28:55 -0600

    express

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
+const express = require("express");
+var bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 4000;
+var userCounter = users.length;
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-
-const { users } = require('./state')
+const { users } = require("./state");
 
 /* BEGIN - create routes here */
+app.get("/users", (req, res) => {
+  res.json(users);
+});
 
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  res.json(users[0]);
+});
+
+app.post("/users/", (req, res) => {
+  let newUser = {
+    _id: 6,
+    name: "Greg",
+    occupation: "Frog Hunter",
+    avatar:
+      "https://sketchok.com/images/articles/01-cartoons/015-over-the-garden-wall/05/10.jpg"
+  };
+  users.push(newUser);
+  res.json(users);
+});
+
+app.put("/users/1", (req, res) => {
+  users[0].occupation = "Speed Racer";
+  res.json(users);
+});
+
+app.delete("/users/1", (req, res) => {
+  res.send("Deleted");
+});
+
+app.post("/users", (req, res) => {
+  let newId = {
+    _id: userCounter + 1,
+    body: req.body
+  };
+
+  res.json(newId);
+});
 
 /* END - create routes here */
 
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
